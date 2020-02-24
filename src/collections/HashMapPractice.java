51f061a53eff204a050f0a06a117544bdc25e514
piypranav <piy.pranav@gmail.com>
2020-02-25 00:09:38 +0530

    HashMap counts number of string

--- src/collections/HashMapPractice.java
+++ src/collections/HashMapPractice.java
@@ -1,9 +1,23 @@
 package collections;
 
+import java.util.HashMap;
+
 public class HashMapPractice {
 
 	public static void main(String[] args) {
+		String sData = "This is my string, is there a need to know the count of words";
 		
+		HashMap<String, Integer> hHashMap = new HashMap<String, Integer>();
+		String[] aString = sData.split(" ");
+		for(String s: aString) {
+			if(hHashMap.containsKey(s)) {
+				hHashMap.put(s, hHashMap.get(s) + 1);
+			} else {
+				hHashMap.put(s, 1);
+			}
+		}
+		
+		System.out.println("HashMap value is: " + hHashMap.entrySet());
 
 	}
 
