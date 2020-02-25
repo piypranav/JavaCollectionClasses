d597d9c75d13b4dca2b1ced9f099196c3a9d8284
piypranav <piy.pranav@gmail.com>
2020-02-25 23:52:54 +0530

    Additional code written to count characters

--- src/collections/HashMapPractice.java
+++ src/collections/HashMapPractice.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 
 public class HashMapPractice {
 	
-	public void hash_map(String sString) {
+	public void hash_map_string(String sString) {
 		HashMap<String, Integer> hHashMap = new HashMap<String, Integer>();
 		String[] aString = sString.split(" ");
 		for(String s: aString) {
@@ -14,14 +14,29 @@ public class HashMapPractice {
 				hHashMap.put(s,  1);
 			}
 		}
-		System.out.println(hHashMap.entrySet());
+		System.out.println("String count is: " + hHashMap.entrySet());
+	}
+	
+	public void hash_map_char(String sString) {
+		char[] aCharArray = sString.toCharArray();
+		HashMap<Character, Integer> hHashMap = new HashMap<Character, Integer>();
+		for(char c: aCharArray) {
+			if(hHashMap.containsKey(c)) {
+				hHashMap.put(c, hHashMap.get(c) + 1);
+			} else {
+				hHashMap.put(c, 1);
+			}
+		}
+		System.out.println("Characters count are: " + hHashMap.entrySet());
+		
 	}
 
 	public static void main(String[] args) {
 		String sData = "This is my string, is there a need to know the count of words";
 		
 		HashMapPractice hmp = new HashMapPractice();
-		hmp.hash_map(sData);
+		hmp.hash_map_string(sData);
+		hmp.hash_map_char(sData);
 		
 	}
 
