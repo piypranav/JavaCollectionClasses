e948dd931e5058f8e23e2c1c50df69ab1a9ac5fb
piypranav <piy.pranav@gmail.com>
2020-02-25 23:40:57 +0530

    New commit to HashMap code

--- src/collections/HashMapPractice.java
+++ src/collections/HashMapPractice.java
@@ -3,22 +3,26 @@ package collections;
 import java.util.HashMap;
 
 public class HashMapPractice {
+	
+	public void hash_map(String sString) {
+		HashMap<String, Integer> hHashMap = new HashMap<String, Integer>();
+		String[] aString = sString.split(" ");
+		for(String s: aString) {
+			if(hHashMap.containsKey(s)) {
+				hHashMap.put(s,  hHashMap.get(s) + 1);
+			} else {
+				hHashMap.put(s,  1);
+			}
+		}
+		System.out.println(hHashMap.entrySet());
+	}
 
 	public static void main(String[] args) {
 		String sData = "This is my string, is there a need to know the count of words";
 		
-		HashMap<String, Integer> hHashMap = new HashMap<String, Integer>();
-		String[] aString = sData.split(" ");
-		for(String s: aString) {
-			if(hHashMap.containsKey(s)) {
-				hHashMap.put(s, hHashMap.get(s) + 1);
-			} else {
-				hHashMap.put(s, 1);
-			}
-		}
+		HashMapPractice hmp = new HashMapPractice();
+		hmp.hash_map(sData);
 		
-		System.out.println("HashMap value is: " + hHashMap.entrySet());
-
 	}
 
 }
